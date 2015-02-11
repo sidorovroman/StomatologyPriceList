0b02413d7be77b53a3b8765f1c8666b603355979
Роман петрович <sidorovroman@some-2.local>
2015-02-12 00:24:34 +0400

    text area autoresize; add doctors datalist; count result sum;

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+    $('.table thead tr').children().eq(-2).width(70);
+
     $('#datepicker').datepicker({
         autoclose: true,
         todayBtn:'linked',
@@ -6,16 +8,30 @@ $(function() {
         todayHighlight: true,
         language: "ru"
     }).datepicker("setDate", new Date());
+    $("#diagnosis").autosize();
     $('.table tbody tr').addClass("no-print");
     $('.table tbody tr').click(function(){
         $(this).toggleClass('success no-print');
-        result();
+        if($(this).hasClass('success')){
+            $(this).children().eq(-2).append("<input type='number' class='service-count' value='1'>");
+        }else{
+            $(this).children().eq(-2).empty();
+        }
+        getResult();
     });
 });
-function result() {
-    this._result = $('.result');
+
+function getResult() {
     var rowArray = $('.table tbody tr.success');
+    var values = [];
     for (var i = 0; i < rowArray.length; i++){
-        console.log($(rowArray[i]).children().last());
+        values.push(+($(rowArray[i]).children().last().children().text()));
     }
+    var total = 0;
+    for (var j = 0; j < values.length; j++) {
+        total += values[j];
+    }
+
+    this._result = $('#result');
+    this._result.val(total);
 }
